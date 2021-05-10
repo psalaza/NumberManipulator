3c10638c8d58ff97cdbd1b1f7e33d0a359bbb072
psalaza <43583947+psalaza@users.noreply.github.com>
2021-05-10 16:39:53 -0400

    Complete

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 12;
+
+            Console.WriteLine(myInt);
+
+            myInt *= myInt;
+            myInt += 14;
+            myInt -= 3;
+
+            Console.WriteLine(myInt);
+
+
+        }
+    }
+}
